764011702d7733d14a59f002ceb1c691554c3a88
Steven Morad <smorad@ucsc.edu>
2014-02-02 12:32:51 -0800

    Update schedule.c

--- schedule.c
+++ schedule.c
@@ -298,6 +298,7 @@ PRIVATE void balance_queues(struct timer *tp)
 			if(winning_num <= 0){
 				rmp->priority = WINNER_Q;	/*winner!*/
 				printf("winner is %d\n", proc_nr);
+				return;
 			}
 			
 			else{
