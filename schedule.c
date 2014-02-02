25aea1ed9598a7c6fa86c6bc306b248b8c2f2509
Steven Morad <smorad@ucsc.edu>
2014-02-02 12:48:31 -0800

    fixed is_user_proc

--- schedule.c
+++ schedule.c
@@ -26,7 +26,7 @@ FORWARD _PROTOTYPE( void balance_queues, (struct timer *tp)		);
 /*#define DYNAMIC_PRIORITY*/
 
 PRIVATE int is_user_proc(int prio){
-	return (prio < WINNER_Q);
+	return (prio > WINNER_Q);
 }
 
 unsigned max_tickets;
