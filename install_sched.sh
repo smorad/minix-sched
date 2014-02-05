df30824e2d0d6fc285f016199cd7afa0f76023a9
Steven Morad <smorad@ucsc.edu>
2014-02-04 16:59:34 -0800

    Update install_sched.sh

--- install_sched.sh
+++ install_sched.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#cd /root/
+#curl -kL https://d1b10bmlvqabco.cloudfront.net/attach/hpvmf4tj33s6oq/hm2ffxrreaxur/hr1mnbt11oad/minixR3.1.8tocommit300773cdca8cae8a8bb9a50d649e6b2a3d445b3a.patch.tar.gz | tar -xvf patch
+#cd /usr/src
+#patch -p1 < /root/patch
 cd /usr/src/servers/sched
 curl -kL https://raw.github.com/smorad/minix-sched/master/schedule.c | tee schedule.c
 curl -kL https://raw.github.com/smorad/minix-sched/master/schedproc.h | tee schedproc.h
